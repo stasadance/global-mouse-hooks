879a002a652494d2d392c41d86f31b5c96257492
coldkey <samsono@gmail.com>
2020-03-03 15:41:44 -0500

    add dist folder

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="node" />
+import { EventEmitter } from "events";
+declare class MouseEvents extends EventEmitter {
+    constructor();
+}
+declare const mouseEvents: MouseEvents;
+export default mouseEvents;
